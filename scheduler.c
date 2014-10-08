718c5aca83ae67957854a5fb1917b0a99b916a17
Jackson Burlew <jackburlew5@gmail.com>
2014-10-08 18:08:54 -0400

    FCFS working.

--- scheduler.c
+++ scheduler.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <math.h>
 
 struct node {
 	int tid;
 	int remainingTime;
-	int currentTime;
+	float currentTime;
 	int tprio;
 	pthread_cond_t sleep;
 	struct node* next;
@@ -14,7 +15,7 @@ struct node {
 struct node* Ready[5];
 struct node* Executing;
 int (*Scheduler)(float, int, int, int);
-float CurrentTime = 0.0f;
+float CurrentTime = -1.0f;
 pthread_cond_t WakeUp;
 pthread_mutex_t SchedMutex;
 
@@ -26,7 +27,6 @@ static void addQueueCurrentTimeIncreasing (struct node* toAdd) {
 
 	if (head == NULL) {
 		Ready[toAdd->tprio] = toAdd;
-		printf("\taddQueue (beginning): %d\n", toAdd->tid);
 		return;
 	}
 	while (head != NULL && head->currentTime < toAdd->currentTime) {
@@ -34,11 +34,9 @@ static void addQueueCurrentTimeIncreasing (struct node* toAdd) {
 		head = head->next;
 	}
 	if (prev == head) {
-		printf("\taddQueue (beginning, precedes %d): %d\n", head->tid, toAdd->tid);
 		toAdd->next = head;
 		Ready[toAdd->tprio] = toAdd;
 	} else {
-		printf("\taddQueue (succeeds %d, precedes %d): %d\n", prev->tid, head->tid, toAdd->tid);
 		toAdd->next = head;
 		prev->next = toAdd;
 	}
@@ -59,13 +57,13 @@ int fcfs(float currentTime, int tid, int remainingTime, int tprio) {
 	// Acquire mutex.
 	pthread_mutex_lock(&SchedMutex);
 
-	fprintf(stderr, "t: %f; tid: %d; remainingTime: %d", currentTime, tid, remainingTime);
+//	fprintf(stderr, "t: %f; tid: %d; remainingTime: %d", currentTime, tid, remainingTime);
 
 	// Identify if this is the first time the thread is requesting to be scheduled.
 	// If it does not match with the thread "Executing", then it is.
 	struct node* thread = NULL;
 	if (Executing == NULL || tid != Executing->tid) {
-		fprintf(stderr, " (first arrival)\n");
+//		fprintf(stderr, " (first arrival)\n");
 		// Create a 'struct node' value to represent this thread.
 		thread = malloc(sizeof(struct node));
 		thread->remainingTime = remainingTime;
@@ -75,7 +73,7 @@ int fcfs(float currentTime, int tid, int remainingTime, int tprio) {
 		pthread_cond_init(&thread->sleep, NULL);
 		addQueueCurrentTimeIncreasing(thread);
 	} else {
-		fprintf(stderr, "\n");
+//		fprintf(stderr, "\n");
 		thread = Executing;
 		thread->remainingTime = remainingTime;
 	}
@@ -83,31 +81,32 @@ int fcfs(float currentTime, int tid, int remainingTime, int tprio) {
 	// If the thread executing should not be (that is, its 'currentTime' value
 	// is greater than some other's), then signal to the next one and enter the
 	// sleeping state.
-	if (Ready[0] != NULL)
-		fprintf(stderr, "Arrived [%d] to find ready queue holding: %d\n", tid, Ready[0]->tid);
-	else
-		fprintf(stderr, "Arrived [%d] to find empty ready queue.\n", tid);
 	if (Ready[0]->tid != tid) {
 		pthread_cond_signal(&Ready[0]->sleep);
 		pthread_cond_wait(&thread->sleep, &SchedMutex);
 	}
 
 	// If this thread is done executing, remove it from the mix.
-	if (remainingTime == 0) {
+	if (thread->remainingTime == 0) {
 		popQueue(0);
+	} else {
+		CurrentTime++;
+		// If the thread is not ready to run, 'yield' for some amount of time.
+		// For instance, the CurrentTime could be 12, yet the arrival time is
+		// 12.1. As we're assuming non-divisible time slots, we should round up
+		// to 13.
+		if (CurrentTime < thread->currentTime) {
+			CurrentTime += ceil(-CurrentTime + thread->currentTime);
+		}
 	}
 	Executing = thread;
 	pthread_mutex_unlock(&SchedMutex);
 
 	// If this thread is done executing, send in a signal to the next thread in line.
-//	fprintf(stderr, "remainingTime: %d\n", thread->remainingTime);
 	if (remainingTime == 0) {
 		if (Ready[0] != NULL)
 			pthread_cond_signal(&Ready[0]->sleep);
 	}
-	CurrentTime++;
-
-	fprintf(stderr, "returning %d with current time: %f\n", tid, CurrentTime);
 
 	return CurrentTime;
 }
