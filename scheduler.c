f598cf52e034adf7e9489b600f7b80f9733c50a9
Jackson Burlew <jackburlew5@gmail.com>
2014-10-12 16:35:29 -0400

    Merged srtf with other functions.

--- scheduler.c
+++ scheduler.c
@@ -18,6 +18,7 @@ struct node {
 	int tid;
 	int remainingTime;
 	float currentTime;
+	int timeQuantum;
 	int tprio;
 	pthread_cond_t sleep;
 	struct node* next;
@@ -29,8 +30,54 @@ int (*Scheduler)(float, int, int, int);
 float CurrentTime = -1.0f;
 pthread_mutex_t SchedMutex;
 
-static void sortQueueRemainingTimeIncreasing ()
-{
+static void debug(int level, const char *fmt, ...) {
+	if (!(level & DEBUG_LEVEL))
+		return;
+	va_list args;
+	va_start(args, fmt);
+
+	// Provide color coding.
+	fprintf(stderr, "\033[%dm", 31 + (int) log2(level));
+	vfprintf(stderr, fmt, args);
+	fprintf(stderr, CTRL_WHITE);
+	va_end(args);
+}
+
+static void print_ready_queues() {
+	debug(1, "Ready Queues:\n");
+	for (int i = 0; i < MAX_PRIORITY; i++) {
+		debug(1, "   Priority: %d\n", i + 1);
+		struct node* head = Ready[i];
+		while (head != NULL) {
+			debug(1, "      tid: %d; currentTime: %f; remainingTime: %d\n", head->tid, head->currentTime, head->remainingTime);
+			head = head -> next;
+		}
+	}
+}
+
+static void addQueueCurrentTimeIncreasing (struct node* toAdd) {
+	struct node* head = Ready[toAdd->tprio];
+	struct node* prev = head;
+	toAdd->next = NULL;
+
+	if (head == NULL) {
+		Ready[toAdd->tprio] = toAdd;
+		return;
+	}
+	while (head != NULL && head->currentTime < toAdd->currentTime) {
+		prev = head;
+		head = head->next;
+	}
+	if (prev == head) {
+		toAdd->next = head;
+		Ready[toAdd->tprio] = toAdd;
+	} else {
+		toAdd->next = head;
+		prev->next = toAdd;
+	}
+}
+
+static void sortQueueRemainingTimeIncreasing () {
 	int i;
 	int flag = 0;
 	for(i = 0; i < 5; i++)
@@ -94,7 +141,6 @@ static void sortQueueRemainingTimeIncreasing ()
 			}
 		}
 	}
-	
 }
 
 static void addQueueRemainingTimeIncreasing (struct node* toAdd) {
@@ -125,52 +171,21 @@ static void addQueueRemainingTimeIncreasing (struct node* toAdd) {
 		prev->next = toAdd;
 	}
 }
-	
-static void debug(int level, const char *fmt, ...) {
-	if (!(level & DEBUG_LEVEL))
-		return;
-	va_list args;
-	va_start(args, fmt);
 
-	// Provide color coding.
-	fprintf(stderr, "\033[%dm", 31 + (int) log2(level));
-	vfprintf(stderr, fmt, args);
-	fprintf(stderr, CTRL_WHITE);
-	va_end(args);
-}
+static int inQueue (int tid) {
+	int i;
+	for(i = 0; i < 5; i++)
+	{
+		struct node *head = Ready[i];
 
-static void print_ready_queues() {
-	debug(1, "Ready Queues:\n");
-	for (int i = 0; i < MAX_PRIORITY; i++) {
-		debug(1, "   Priority: %d\n", i + 1);
-		struct node* head = Ready[i];
-		while (head != NULL) {
-			debug(1, "      tid: %d; currentTime: %f; remainingTime: %d\n", head->tid, head->currentTime, head->remainingTime);
-			head = head -> next;
+		while(head != NULL)
+		{
+			if(head->tid == tid)
+				return 1;
+			head = head->next;
 		}
 	}
-}
-
-static void addQueueCurrentTimeIncreasing (struct node* toAdd) {
-	struct node* head = Ready[toAdd->tprio];
-	struct node* prev = head;
-	toAdd->next = NULL;
-
-	if (head == NULL) {
-		Ready[toAdd->tprio] = toAdd;
-		return;
-	}
-	while (head != NULL && head->currentTime < toAdd->currentTime) {
-		prev = head;
-		head = head->next;
-	}
-	if (prev == head) {
-		toAdd->next = head;
-		Ready[toAdd->tprio] = toAdd;
-	} else {
-		toAdd->next = head;
-		prev->next = toAdd;
-	}
+	return 0;
 }
 
 static void popQueue (int tprio) {
@@ -204,7 +219,6 @@ static void removeFromQueue (struct node* toRemove) {
 			prev->next = head->next;
 		}
 	}
-	free(toRemove);
 }
 
 static void bubbleSortCurrentTime(int queueIndex) {
@@ -254,7 +268,7 @@ int fcfs(float currentTime, int tid, int remainingTime, int tprio) {
 		pthread_cond_init(&thread->sleep, NULL);
 		addQueueCurrentTimeIncreasing(thread);
 	} else {
-		if(Executing == NULL) Executing = Ready[0];
+		debug(1, "\n");
 		thread = Executing;
 		thread->remainingTime = remainingTime;
 	}
@@ -272,7 +286,6 @@ int fcfs(float currentTime, int tid, int remainingTime, int tprio) {
 	// If this thread is done executing, remove it from the mix.
 	if (thread->remainingTime == 0) {
 		popQueue(0);
-		Executing = NULL;
 	} else {
 		CurrentTime++;
 		// If the thread is not ready to run, 'yield' for some amount of time.
@@ -282,8 +295,9 @@ int fcfs(float currentTime, int tid, int remainingTime, int tprio) {
 		if (CurrentTime < thread->currentTime) {
 			CurrentTime += ceil(-CurrentTime + thread->currentTime);
 		}
-		Executing = thread;
 	}
+	Executing = thread;
+	debug(2, "Releasing mutex [%d] (scheduleme finished)\n", tid);
 	pthread_mutex_unlock(&SchedMutex);
 
 	// If this thread is done executing, send in a signal to the next thread in line.
@@ -295,33 +309,18 @@ int fcfs(float currentTime, int tid, int remainingTime, int tprio) {
 	return CurrentTime;
 }
 
-int inQueue (int tid)
-{
-	int i;
-	for(i = 0; i < 5; i++)
-	{
-		struct node *head = Ready[i];
-
-		while(head != NULL)
-		{
-			if(head->tid == tid)
-				return 1;
-			head = head->next;
-		}
-	}
-	return 0;
-}
-
 int srtf(float currentTime, int tid, int remainingTime, int tprio) {
 	// Acquire mutex.
 	pthread_mutex_lock(&SchedMutex);
+	debug(8, "gaining mutex (entering scheduleme): %d\n", tid);
 
 	//printf("Entered srtf [%d]\n", tid);
 	// Identify if this is the first time the thread is requesting to be scheduled.
 	// If it does not match with the thread "Executing", then it is.
+	debug(4, "t: %f; tid: %d; remainingTime: %d", currentTime, tid, remainingTime);
 	struct node* thread = NULL;
-	if (!inQueue(tid)) {
-		//fprintf(stderr, " (first arrival)\n");
+	if (Executing == NULL || tid != Executing->tid) {
+		debug(4, " (first arrival)\n");
 		// Create a 'struct node' value to represent this thread.
 		thread = malloc(sizeof(struct node));
 		thread->remainingTime = remainingTime;
@@ -331,11 +330,18 @@ int srtf(float currentTime, int tid, int remainingTime, int tprio) {
 		pthread_cond_init(&thread->sleep, NULL);
 		addQueueRemainingTimeIncreasing(thread);
 	} else {
-		if(Executing == NULL) Executing = Ready[0];
+		debug(4, "\n");
 		thread = Executing;
 		thread->remainingTime = remainingTime;
 		thread->currentTime = CurrentTime + 1;
 		sortQueueRemainingTimeIncreasing();
+	}
+	print_ready_queues();
+
+	if (Executing != thread && Executing != NULL) {
+		debug(8, "releasing mutex (early sleep): %d\n", tid);
+		pthread_cond_wait(&thread->sleep, &SchedMutex);
+		debug(8, "gaining mutex (signaled): %d\n", tid);
 	}
 
 	// If the thread executing should not be (that is, its 'currentTime' value
@@ -343,7 +349,9 @@ int srtf(float currentTime, int tid, int remainingTime, int tprio) {
 	// sleeping state.
 	if (Ready[0]->tid != tid) {
 		pthread_cond_signal(&Ready[0]->sleep);
+		debug(8, "releasing mutex (sleep): %d\n", tid);
 		pthread_cond_wait(&thread->sleep, &SchedMutex);
+		debug(8, "gaining mutex (signaled): %d\n", tid);
 	}
 
 	// If this thread is done executing, remove it from the mix.
@@ -361,6 +369,7 @@ int srtf(float currentTime, int tid, int remainingTime, int tprio) {
 		}
 		Executing = thread;
 	}
+	debug(8, "releasing mutex (time to execute): %d\n", tid);
 	pthread_mutex_unlock(&SchedMutex);
 
 	// If this thread is done executing, send in a signal to the next thread in line.
@@ -401,6 +410,7 @@ int pbs(float currentTime, int tid, int remainingTime, int tprio) {
 	if (remainingTime == 0) {
 		Executing = NULL;
 		removeFromQueue(thread);
+		free(thread);
 	}
 	print_ready_queues();
 	// Check to see if the current thread is the one that should be running. If
@@ -470,7 +480,103 @@ int pbs(float currentTime, int tid, int remainingTime, int tprio) {
 }
 
 int mlfq(float currentTime, int tid, int remainingTime, int tprio) {
+	pthread_mutex_lock(&SchedMutex);
 
+	debug(8, "gaining mutex (entering scheduleme): %d\n", tid);
+	debug(4, "t: %f; tid: %d; remainingTime: %d", currentTime, tid, remainingTime);
+
+	// Get a pointer to the thread's representation. This might include creating
+	// a new one if need be.
+	struct node* thread = NULL;
+	if (Executing==NULL || Executing->tid != tid) {
+		// Create a 'struct node' value to represent this thread.
+		thread = malloc(sizeof(struct node));
+		thread->remainingTime = remainingTime;
+		thread->tid = tid;
+		thread->timeQuantum = 5;
+		thread->currentTime = currentTime;
+		thread->tprio = 0; // All threads start at highest priority.
+		pthread_cond_init(&thread->sleep, NULL);
+		addQueueCurrentTimeIncreasing(thread);
+		debug(4, " (first arrival)\n");
+	} else {
+		thread = Executing;
+		thread->remainingTime = remainingTime;
+		//thread->currentTime = CurrentTime + 1;
+		//bubbleSortCurrentTime(thread->tprio);
+		debug(4, "\n");
+	}
+
+	// Remove the thread if it is finished. Otherwise, if it has exceeded its
+	// time quantum, push its priority down.
+	if (remainingTime == 0) {
+		debug(16, "Completed thread %d\n", tid);
+		Executing = NULL;
+		removeFromQueue(thread);
+		free(thread);
+		print_ready_queues();
+	} else if (thread->timeQuantum <= 0) {
+		debug(16, "Exceeded time quantum for thread %d\n", tid);
+		removeFromQueue(thread);
+		thread->tprio++;
+		thread->timeQuantum = thread->tprio * 5 + 5;
+		addQueueCurrentTimeIncreasing(thread);
+		print_ready_queues();
+		Executing = NULL;
+	}
+
+	// Pick the thread to schedule next. It should be Ready[0] || Ready[1] || ...
+	// We ARE preempting if a new thread comes in when the currently executing
+	// thread is still executing its time quantum.
+	int i;
+	int lowestTime = -1;
+	debug(32, "Executing: %p\n", Executing);
+	if (Executing != thread && Executing != NULL) {
+		debug(8, "releasing mutex (early sleep): %d\n", tid);
+		pthread_cond_wait(&thread->sleep, &SchedMutex);
+		debug(8, "gaining mutex (signaled): %d\n", tid);
+	}
+	do {
+		for (i = 0; i < MAX_PRIORITY; i++) {
+			if (Ready[i] == thread && Ready[i]->currentTime <= CurrentTime + 1) {
+				thread->timeQuantum--;
+				CurrentTime++;
+				Executing = thread;
+				break;
+			} else if (Ready[i] != NULL && Ready[i]->currentTime <= CurrentTime + 1) {
+				pthread_cond_signal(&Ready[i]->sleep);
+				debug(8, "releasing mutex (yielding): %d\n", tid);
+				pthread_cond_wait(&thread->sleep, &SchedMutex);
+				thread->timeQuantum--;
+				CurrentTime++;
+				Executing = thread;
+				debug(8, "gaining mutex (signaled): %d\n", tid);
+				break;
+			}
+		}
+		debug(2, "i: %d\n", i);
+		// The following will be true if the loop doesn't break (ie, it
+		// executes to completion).
+		if (i == MAX_PRIORITY) {
+			// Check to see if all ready queues are empty. If so, terminate.
+			int k;
+			for (k = 0; k < MAX_PRIORITY && Ready[k] == NULL; k++);
+			if (k == MAX_PRIORITY)
+				break;
+			// Otherwise, increment current time so that it fits the lowest
+			// start time.
+			CurrentTime++;
+			debug(2, "no thread found [%d]\n", tid);
+		} else {
+			break;
+		}
+
+	} while (1);
+
+	debug(8, "releasing mutex (finished): %d\n", tid);
+	pthread_mutex_unlock(&SchedMutex);
+
+	return CurrentTime;
 }
 
 
@@ -497,88 +603,3 @@ void init_scheduler(int schedType) {
 int scheduleme(float currentTime, int tid, int remainingTime, int tprio) {
 	return (*Scheduler)(currentTime, tid, remainingTime, tprio);
 }
-
-//int main ()
-//{
-//  struct node* t1 = malloc(sizeof(struct node));
-//  struct node* t2 = malloc(sizeof(struct node));
-//  struct node* t3 = malloc(sizeof(struct node));
-//  struct node* t4 = malloc(sizeof(struct node));
-//  struct node* t5 = malloc(sizeof(struct node));
-//
-//  t1->tid = 1;
-//  t1->tprio = 0;
-//  t1->remainingTime = 10.0f;
-//  t1->currentTime = 10.0f;
-//  
-//  t2->tid = 2;
-//  t2->tprio = 0;
-//  t2->remainingTime = 20.0f;
-//  t2->currentTime = 20.0f;
-//  
-//  t3->tid = 3;
-//  t3->tprio = 0;
-//  t3->remainingTime = 20.0f;
-//  t3->currentTime = 10.0f;
-//  
-//  t4->tid = 4;
-//  t4->tprio = 0;
-//  t4->remainingTime = 30.0f;
-//  t4->currentTime = 00.0f;
-//  
-//  t5->tid = 5;
-//  t5->tprio = 0;
-//  t5->remainingTime = 10.0f;
-//  t5->currentTime = 30.0f;
-//
-//  addQueueCurrentTimeIncreasing(t1);
-//  struct node* head = Ready[0];
-//  while(head != NULL)
-//  {
-//      printf("TID %d Reamaining %d Current %g\n", head->tid, head->remainingTime, head->currentTime);
-//      head = head->next;
-//  }
-//  printf("\n");
-//  addQueueCurrentTimeIncreasing(t2);
-//  head = Ready[0];
-//  while(head != NULL)
-//  {
-//      printf("TID %d Reamaining %d Current %g\n", head->tid, head->remainingTime, head->currentTime);
-//      head = head->next;
-//  }
-//  printf("\n");
-//  addQueueCurrentTimeIncreasing(t3);
-//  head = Ready[0];
-//  while(head != NULL)
-//  {
-//      printf("TID %d Reamaining %d Current %g\n", head->tid, head->remainingTime, head->currentTime);
-//      head = head->next;
-//  }
-//  printf("\n");
-//  addQueueCurrentTimeIncreasing(t4);
-//  head = Ready[0];
-//  while(head != NULL)
-//  {
-//      printf("TID %d Reamaining %d Current %g\n", head->tid, head->remainingTime, head->currentTime);
-//      head = head->next;
-//  }
-//  printf("\n");
-//  addQueueCurrentTimeIncreasing(t5);
-//  head = Ready[0];
-//  while(head != NULL)
-//  {
-//      printf("TID %d Reamaining %d Current %g\n", head->tid, head->remainingTime, head->currentTime);
-//      head = head->next;
-//  }
-//  printf("\n");
-//
-//  sortQueueRemainingTimeIncreasing();
-//  head = Ready[0];
-//  while(head != NULL)
-//  {
-//      printf("TID %d Reamaining %d Current %g\n", head->tid, head->remainingTime, head->currentTime);
-//      head = head->next;
-//  }
-//
-//  return 0;
-//}
